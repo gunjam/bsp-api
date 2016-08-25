2ef307846cbc741be0f27356f978eaaac22b0336
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-08-25 13:16:31 +0100

    First commit, kinda right API

--- common/models/payment.js
+++ common/models/payment.js
@@ -1,3 +1,12 @@
-module.exports = function (Payment) {
+function isFutureDate(err) {
+  const now = new Date();
+  if (this.dateOfPayment < now) {
+    err();
+  }
+}
 
+module.exports = function (Payment) {
+  Payment.validate('date', isFutureDate, {
+    message: {with: 'Must be future date'}
+  });
 };
--- common/models/schedule.js
+++ common/models/schedule.js
@@ -1,36 +1,18 @@
 const validNino = /^(?!BG|GB|NK|KN|TN|NT|ZZ)[ABCEGHJ-PRSTW-Z]{2}\d{6}[A-D]$/i;
 const validSortCode = /^\d{2}-\d{2}-\d{2}$/;
-
-function isNationalInsuranceNumber(err) {
-  if (!isNino.test(this.nationalInsuranceNumber)) {
-    err();
-  }
-}
-
-function isSortCode(err) {
-  if (!validSortCode.test(this.sortCode)) {
-    err();
-  }
-}
-
-function isFutureDate(err) {
-  const now = new Date();
-  if (this.dateOfPayment < now) {
-    err();
-  }
-}
+const validAccountNumber = /^\d{8}$/;
 
 module.exports = function(Schedule) {
   Schedule.validatesFormatOf('nationalInsuranceNumber', {
-    with: isNationalInsuranceNumber,
-    message: {with: 'Sort code must be 6 numbers'}
+    with: validNino,
+    message: {with: 'National Insurance number is not in the correct format'}
   });
   Schedule.validatesFormatOf('sortCode', {
-    with: isSortCode,
+    with: validSortCode,
     message: {with: 'Sort code must be in the format NN-NN-NN'}
   });
-  Schedule.validatesLengthOf('accountNumber', {
-    is: 8,
+  Schedule.validatesFormatOf('accountNumber', {
+    with: validAccountNumber,
     message: {is: 'Account number must be 8 digits'}
   });
 };
