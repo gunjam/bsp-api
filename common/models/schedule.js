8928adae5ef7cdd2b05f909d441ed917820df483
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-26 18:37:55 +0100

    Add statues to schedules

--- common/models/schedule.js
+++ common/models/schedule.js
@@ -7,4 +7,20 @@ module.exports = function(Schedule) {
     with: validNino,
     message: {with: 'National Insurance number is not in the correct format'}
   });
+  Schedule.remoteMethod('addStatus', {
+    accepts: [
+      {arg: 'name', type: 'string'},
+      {arg: 'reason', type: 'string'}
+    ],
+    isStatic: false,
+    description: 'Add a status to a schedule',
+    returns: {type: 'Schedule', root: true},
+    http: {path: '/addStatus', verb: 'put', status: 200}
+  });
+  Schedule.prototype.addStatus = function(name, reason, cb) {
+    const data = {name, reason};
+    const statuses = this.statuses;
+    statuses[statuses.length - 1].endDate = new Date();
+    this.updateAttribute('statuses', [...statuses, data], (err, instance) => cb(err, data));
+  };
 };
