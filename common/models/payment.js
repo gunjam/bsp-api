2435ea105de2c580c7d7c7dc635533bb599fa0b3
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-26 11:21:34 +0100

    Add support for payment types

--- common/models/payment.js
+++ common/models/payment.js
@@ -5,8 +5,14 @@ function isFutureDate(err) {
   }
 }
 
+const isValidType = /^initial$|^monthly$|^backDated$/;
+
 module.exports = function (Payment) {
   Payment.validate('date', isFutureDate, {
     message: {with: 'Must be future date'}
+  });
+  Payment.validatesFormatOf('type', {
+    with: isValidType,
+    message: {is: 'Payment type must be either initial, monthly or backDated'}
   });
 };
