0fe11f105af90ebd3378b4799ddfc164b5466238
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-09-28 16:34:38 +0100

    Payments persist as models, bank details separate, editable

--- common/models/schedule.js
+++ common/models/schedule.js
@@ -7,6 +7,7 @@ module.exports = function(Schedule) {
     with: validNino,
     message: {with: 'National Insurance number is not in the correct format'}
   });
+
   Schedule.remoteMethod('addStatus', {
     accepts: [
       {arg: 'name', type: 'string'},
@@ -17,10 +18,34 @@ module.exports = function(Schedule) {
     returns: {type: 'Schedule', root: true},
     http: {path: '/addStatus', verb: 'put', status: 200}
   });
+
   Schedule.prototype.addStatus = function(name, reason, cb) {
     const data = {name, reason};
     const statuses = this.statuses;
     statuses[statuses.length - 1].endDate = new Date();
     this.updateAttribute('statuses', [...statuses, data], (err, instance) => cb(err, data));
+  };
+
+  Schedule.remoteMethod('saveFull', {
+    accepts: [
+      {arg: 'nationalInsuranceNumber', type: 'string'},
+      {arg: 'paymentSchedule', type: 'object'}
+    ],
+    description: 'Add and save a full schedule all in one go',
+    returns: {type: 'Schedule', root: true}
+  });
+
+  Schedule.saveFull = function(nationalInsuranceNumber, paymentSchedule, cb) {
+    const Payment = Schedule.app.models.Payment;
+
+    Schedule.create({nationalInsuranceNumber}, function(err, schedule) {
+      if (err) {
+        return cb(err);
+      }
+      const scheduleId = schedule.id;
+      const linkedSchedule = paymentSchedule.map(i => Object.assign(i, {scheduleId}));
+
+      Payment.create(linkedSchedule, (err, payments) => cb(err, schedule));
+    });
   };
 };
